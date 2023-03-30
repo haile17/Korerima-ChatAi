79b3bd00e72c9f5faff639c66a68dbb8c1e1c03d
Haile Fikere <hailefikere17@gmail.com>
2023-03-30 16:39:36 +0300

    update

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-  const response = await fetch('https://korerima-chatai-p126.onrender.com', {
+  const response = await fetch('https://korerima-chatai.onrender.com', {
     method: 'POST',
         headers: {
             'Content-Type': 'application/json',
